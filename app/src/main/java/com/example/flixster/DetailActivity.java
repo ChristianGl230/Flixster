8f9584d960ab20def81c2ed5c618958fe0f1e5be
lilgoode <christian.goode@bison.howard.edu>
2021-09-28 11:36:05 -0400

    flixster

--- app/src/main/java/com/example/flixster/DetailActivity.java
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -25,7 +25,7 @@ public class DetailActivity extends YouTubeBaseActivity {
 
 
     private static final String YOUTUBE_API_KEY = "AIzaSyAFQJ8lZjYVvsH3TC6I-kuuMYznS2JBv0o";
-    public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4c";
+    public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%s/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     TextView tvTitle;
     TextView tvOverview;
